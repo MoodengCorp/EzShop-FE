824bf78b188cc466a45ba39fcaf0249892331749
Dahyeonni <youlove7r@gmail.com>
2025-11-20 16:39:42 +0900

    Feat: Page - 페이지 - 검색 결과 | 카테고리별 조회 페이지 구현
    검색 결과 | 카테고리별 조회 페이지 구현
    
    Resolves: #16
    Ref: #12

--- src/features/item/hooks/useItem.ts
+++ src/features/item/hooks/useItem.ts
@@ -1,44 +1,22 @@
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useQuery, keepPreviousData } from '@tanstack/react-query'
 import { itemKeys } from '../api/queryKeys'
 import { itemApi } from '../api/itemApi'
-import { ItemCreateRequest } from '../types/item'
+import { ItemSearchParams } from '../types/item'
 
-export const useItemsQuery = () => {
+// 파라미터를 받는 형태로 수정
+export const useItems = (params?: ItemSearchParams) => {
   return useQuery({
-    queryKey: itemKeys.details(),
-    queryFn: itemApi.getItems,
+    queryKey: itemKeys.list(params),
+    queryFn: () => itemApi.getItems(params),
+    placeholderData: keepPreviousData,
   })
 }
-
-// B. 상품 상세 조회 Hook
+// 상품 상세 조회 Hook
 export const useItemDetailQuery = (itemId: number) => {
   return useQuery({
     // itemId가 유효할 때만 쿼리 실행
     queryKey: itemKeys.detail(itemId),
     queryFn: () => itemApi.getItemDetail(itemId),
     enabled: !!itemId,
-  })
-}
-
-// C. 상품 생성 Mutation Hook
-export const useCreateItemMutation = () => {
-  // 생성 후 리스트나 다른 캐시를 무효화할 수 있으나, 여기서는 간단히 구현합니다.
-  return useMutation({
-    mutationFn: itemApi.createItem,
-    // onSuccess: (data) => {
-    //   queryClient.invalidateQueries({ queryKey: itemKeys.lists() });
-    // }
-  })
-}
-
-export const useUpdateItemMutation = () => {
-  return useMutation({
-    mutationFn: ({
-      itemId,
-      data,
-    }: {
-      itemId: number
-      data: Partial<ItemCreateRequest>
-    }) => itemApi.updateItem(itemId, data),
   })
 }
