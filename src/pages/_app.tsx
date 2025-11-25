932ad5904fdf996a5f7ad78eba800379b63b6512
Dahyeonni <youlove7r@gmail.com>
2025-11-25 12:16:57 +0900

    Fix: 탭 이름 고정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,17 @@ import Layout from '@/components/layout/Layout'
 import { Toaster } from 'sonner'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/lib/queryClient'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>EzShop</title>
+      </Head>
       <QueryClientProvider client={queryClient}>
         <Layout>
-          <Toaster/>
+          <Toaster />
           <Component {...pageProps} />
         </Layout>
       </QueryClientProvider>
